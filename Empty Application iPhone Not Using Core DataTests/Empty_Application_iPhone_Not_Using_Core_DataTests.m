827f1775149c19a1afd0c6b1e2437383f9f027c6
dfi <ppl@MacM.local>
2014-10-22 17:40:26 +0800

    Initial Commit

---
+++ Empty Application iPhone Not Using Core DataTests/Empty_Application_iPhone_Not_Using_Core_DataTests.m
@@ -0,0 +1,34 @@
+//
+//  Empty_Application_iPhone_Not_Using_Core_DataTests.m
+//  Empty Application iPhone Not Using Core DataTests
+//
+//  Created by ppl on 10/22/14.
+//  Copyright (c) 2014 self.edu. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Empty_Application_iPhone_Not_Using_Core_DataTests : XCTestCase
+
+@end
+
+@implementation Empty_Application_iPhone_Not_Using_Core_DataTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
